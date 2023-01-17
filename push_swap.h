1fac523b4343da560fc60e878b2131ea353e4636
Bilal Salih Nurtas <52494435+bilalnrts@users.noreply.github.com>
2023-01-17 10:20:35 +0300

    Basic pattern created

---
+++ push_swap.h
@@ -0,0 +1,17 @@
+#ifndef PUSH_SWAP_H
+#define PUSH_SWAP_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+typedef struct s_stack
+{
+    int *a_stack;
+    int *b_stack;
+    int a_size;
+    int b_size;
+} t_stack;
+
+
+#endif
