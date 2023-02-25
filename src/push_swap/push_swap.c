6d67d5a30759ef6f27f7dcd6412799b5569328b6
Bilal Salih Nurtas <52494435+bilalnrts@users.noreply.github.com>
2023-02-25 18:26:19 +0300

    There are some changes in work.
    Added some extra functions owing to a mistake in project.

--- src/push_swap/push_swap.c
+++ src/push_swap/push_swap.c
@@ -39,6 +39,26 @@ void	ft_push_swap(char **av)
 	free(stack.b);
 }
 
+char	*ft_conc_values(char **av)
+{
+	int		i;
+	char	*str;
+
+	i = 0;
+	while(av[i])
+	{
+		if (ft_strncmp(av[i], "", 2) == 0)
+		{
+			ft_printf("Error.\n");
+			exit(0);
+		}
+		str = ft_strjoin(str, av[i]);
+		str = ft_strjoin(str, " ");
+		i++;
+	}
+	return (str);
+}
+
 int	main(int ac, char **av)
 {
 	if (ac > 1)
@@ -46,6 +66,8 @@ int	main(int ac, char **av)
 		av++;
 		if (ac == 2)
 			av = ft_split(*av, ' ');
+		else
+			av = ft_split(ft_conc_values(av), ' ');
 		ft_push_swap(av);
 	}
 	return (0);
